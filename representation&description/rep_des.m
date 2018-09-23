f0597bc3d85f33e087d22a22a6e1252c35b2b294
R.Guo <ray.guo.inchina@gmail.com>
2018-09-23 23:18:40 +0800

    representation and description

---
+++ representation&description/rep_des.m
@@ -0,0 +1,15 @@
+% Representation and Description
+% Image representation and description is critical for successful detection 
+% and recognition of objects in a sence
+% After an image has been segmented into objects and background regions,
+% one intends to represent and describe them in characteristic features for
+% computer processing during pattern recognition or in quantitative codes
+% for efficient storage during image compression.
+
+% Representing region involves two choices:
+% 1. in terms of external characteristics(boundary)
+% It mainly deals with shape characteristics
+% 2. in terms of its internal characteristics(pixels comprising the region)
+% It mainly deals with regional properties such as color or texture
+
+% Here some core mthods
